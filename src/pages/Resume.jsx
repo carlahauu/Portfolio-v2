b3e02e712d31633bef9ee49ad9f75f1fc581d1cb
carlahauu <carlahau8@gmail.com>
2025-08-17 02:29:12 -0700

    Updated styling of navbar for mobile and PDF height

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -5,11 +5,12 @@ function Resume() {
   return (
     <div>
         <h3>Want a copy of my resume? <a style={{color: "#9BB657", textDecoration: "underline"}} href={ResumePDF}>Click here to download my resume!</a></h3>
-        <object
+        <object 
+            style={{backgroundColor: "white"}}
             data={ResumePDF}
             type="application/pdf"
             width="100%"
-            height="1100vh"
+            height="550px"
         >
         </object>
     </div>
